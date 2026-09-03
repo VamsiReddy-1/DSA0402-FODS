f735eb5d7bfb7007a5d201abeda2babf7fb4ba81
VamsiReddy-1 <vamsi2530@gmail.com>
2026-09-03 07:18:26 +0530

    Create program.py

---
+++ IET ASSIGNMENT/program.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from scipy import stats
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+…)
